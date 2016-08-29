7e831ee513b86207a37a1bf38d645215189e0803
sirtopas <sirtopas@gmail.com>
2016-08-29 19:44:12 +0100

    Added a character
    Added some background clouds, movement script and moved the ground

---
+++ Source/Fishin Buddies/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	bool facingRight = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis("Horizontal");
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, 0);
+
+		if (move > 0 && !facingRight)
+		{
+			Flip();
+		}
+		else if(move < 0 && facingRight) {
+			Flip();
+		}
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
